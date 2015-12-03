b49963b2c5d9937665db39b0f982e507d07b4f8a
Oscar <oscarwinglee@gmail.com>
2015-12-02 19:00:54 -0500

    Led setups/ IO ports

--- Project4.c
+++ Project4.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <math.h>
-#include <assert.h>
 #include <stdlib.h>
 #include <avr/eeprom.h>
 #include <avr/io.h>
 #include <avr/delay.h>
 
  //Configure timers/USART/interrupts/etc
+
+#define CLOCK 16000000
+#define BAUD 31250 // not sure how to find Baud
 
 // Header Functions
 void receive(void);
@@ -15,23 +16,34 @@ void flushUsart(void);
 void readE2PROM(void);
 void writeE2PROM(void);
 
-}
-
+void init_usart(void);
 //eeprom
 //eeprom_read_block();
 //  uint8_t byteRead = eeprom_read_byte((uint8_t*)23); // read the byte in location 23
 //eeprom_write_block();
 //  eeprom_write_byte ((uint8_t*) 23, 64); //  write the byte 64 to location 23 of the EEPROM
 
-int main(int argc, char *argv[]) {
-	//initialize USART
-	//Enable Receiver and transmitter
-	//initialize to 8 bits, 1 start, 1 stop bit
-
-	//Storage DataEEPROM
+  //Storage DataEEPROM
  	//Communication Usart
  	//Counter Timer1
 
+int main(int argc, char *argv[]) {
+  int record = 0, playback = 0;
+	//initialize USART
+
+  DDRD = 0b00000010; // pin 1: output midi out, pin 0: midi in for storing
+  DDRB = 0b11111111; // LEDs for debugging
+
+  // -- LEDS light up to display startup --
+  PORTB = 0b00000001;
+  _delay_loop_2(1000); //delay of 1s
+  PORTB = 0b00000011;
+  _delay_loop_2(1000);
+  PORTB = 0b00000111;
+  _delay_loop_2(1000);
+  PORTB = 0b00000000;
+
+  // -- Start Listening --
 	while (1) {
 		if (record) {
 			//USART_Read();
