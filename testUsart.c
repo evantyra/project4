d0139f3ed3e5c644799124bf3527bf49c8a39ea1
Oscar Lee <oscarwinglee@gmail.com>
2015-12-05 15:16:18 -0500

    Testing receive works with hardware now

--- testUsart.c
+++ testUsart.c
@@ -5,15 +5,17 @@
 #include <avr/io.h>
 #include <avr/delay.h>
 
-#define CF_CPU 16000000 // 16MHz
+#define CF_CPU 4000000 // 4MHz
 #define BAUD 31250 // not sure how to find Baud
 #define MYUBRR F_CPU/16/BAUD-1
 
 // -- Header Functions --
-// Usart Functions
+// Usart
 void usart_init(uint16_t);
 void usart_putchar(char);
 char usart_getchar(void);
+void usart_flush(void);
+
 // EEPROM
 void readE2PROM(void);
 void writeE2PROM(void);
@@ -26,49 +28,27 @@ int main(int argc, char *argv[]) {
   //PORTD = 0b00000001;
   DDRB = 0b11111111; // LEDs for debugging
 
-  // -- LEDS light up to display startup --
-  PORTB = 0b00000001;
-  _delay_loop_2(65000); //delay of 1s
-  PORTB = 0b00000011;
-  _delay_loop_2(65000);
-  PORTB = 0b00000111;
-  _delay_loop_2(65000);
-  PORTB = 0b00000011;
-  _delay_loop_2(65000);
-  PORTB = 0b00000001;
-  _delay_loop_2(65000);
-  PORTB = 0b00000000;
-  _delay_loop_2(65000);
-
-
   //initialize USART
-  usart_init(MYUBRR);
+  usart_init(7);
 
   // -- Start Listening --
   char status, note, velocity;
   while (1) {
     // Test USART receive
-	usart_putchar(0b00111100);
-    //status = usart_getchar(); // Not sure if it is blocking
-    //note = usart_getchar();
-	//velocity = usart_getchar();
+	//usart_putchar(0b00111100);
+    status = usart_getchar(); // Not sure if it is blocking
+	//usart_putchar('a');
+    note = usart_getchar();
+	velocity = usart_getchar();
 
-	//flushUsart();
-	//flushUsart();
-	//flushUsart();
+	usart_flush();
 
-	PORTB = usart_getchar();
-    _delay_loop_2(65000); // LEDS light for 5s
-    _delay_loop_2(65000);
-	_delay_loop_2(65000);
-	_delay_loop_2(65000);
-	_delay_loop_2(65000);
+	PORTB = note;
 
-	PORTB = 0x00; // Clear Lights
+	//PORTB = 0x00; // Clear Lights
 
     // Test USART send
     //usart_putchar('c');
-    //_delay_loop_2(65000);
 
 	}
 }
@@ -103,6 +83,6 @@ char usart_getchar() {
   return UDR;
 }
 
-void flushUsart() {
+void usart_flush() {
   while ((UCSRA & (1 << UDRE)) == 0); // Waits for buffer to be empty.
 }
